00bf96125af9dd9fb19288ae79a7776b5ec21aac
Ryan Christian <rchristian@ryanchristian.dev>
2025-03-20 21:22:05 -0500

    refactor: Lazy init of config options

--- rplugin/node/vim-package-info/render.js
+++ rplugin/node/vim-package-info/render.js
@@ -8,6 +8,12 @@ export async function clearAll(nvimPlugin) {
     await buffer.clearNamespace({ nsId: 1 });
 }
 
+let initialized = false;
+const configValues = {
+    prefix: '  ¤ ',
+    hlGroup: 'NonText'
+};
+
 /**
  * @param {import('neovim').NvimPlugin} nvimPlugin
  * @param {number} lineNum
@@ -15,26 +21,20 @@ export async function clearAll(nvimPlugin) {
  * @param {string} latest
  */
 export async function drawOne(nvimPlugin, lineNum, current, latest) {
-    const { prefix, hl_group } = await getConfigValues(nvimPlugin);
-    const lp = format(current, prefix, hl_group, latest);
+    if (initialized == false) {
+        try {
+            configValues.prefix = /** @type {string} */ (await nvimPlugin.nvim.eval('g:vim_package_info_virtualtext_prefix'));
+        } catch {}
+        try {
+            configValues.hlGroup = /** @type {string} */ (await nvimPlugin.nvim.eval('g:vim_package_info_virtualtext_highlight'));
+        } catch {}
+
+        initialized = true;
+    }
+
+    const { prefix, hlGroup } = configValues;
+    const lp = format(current, prefix, hlGroup, latest);
 
     const buffer = await nvimPlugin.nvim.buffer;
     await buffer.setVirtualText(1, lineNum, lp);
-}
-
-/**
- * @param {import('neovim').NvimPlugin} nvimPlugin
- */
-async function getConfigValues(nvimPlugin) {
-    let prefix = '  ¤ ';
-    let hl_group = 'NonText';
-
-    try {
-        prefix = await nvimPlugin.nvim.eval('g:vim_package_info_virtualtext_prefix');
-    } catch (error) {}
-    try {
-        hl_group = await nvimPlugin.nvim.eval('g:vim_package_info_virtualtext_highlight');
-    } catch (error) {}
-
-    return { prefix, hl_group };
 }
